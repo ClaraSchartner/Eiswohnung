c7e6bfafc3f3690d27b0a51cd9d270694a68fdf3
unknown <Clara.Schartner@gmail.com>
2019-11-18 20:37:28 +0100

    changed bins

--- 01IceAdress.R
+++ 01IceAdress.R
@@ -27,15 +27,17 @@ leafIce
 
 # Loop making isochrone for every ice cream
 # within the loop union
-bins <- seq(0,8,2)
-for(i in seq_along(Eiscoor$long)){
+bins <- c(0,5,10)
+#for(i in seq_along(Eiscoor$long)){
+ for(i in 1:4){
+   Sys.sleep(rnorm(1,350,40))
 EisIso <- osrmIsochrone(loc =c(Eiscoor$long[i], Eiscoor$lat[i]), 
                         breaks = bins, 
                         returnclass="sf")
 # rest, so that API does not block
-Sys.sleep(rnorm(1,150,10))
+
  # combine the current polygon with the polygon containing the other ice creams
-if( i >1){
+if( i ==1){
   EisAlle <- EisIso %>% select(min)
 }
 EisAlle <- st_union(EisIso, EisAlle)
@@ -43,14 +45,13 @@ EisAlle <- st_union(EisIso, EisAlle)
 EisAlle$min <- apply(EisAlle,1, function(x)min(x$min, x$min.1))
 EisAlle<- select(EisAlle, min)
 }
-
 # visualise
 pal <- colorNumeric(
-  palette = "Reds",
+  palette = "Blues",
   domain = EisAlle$min)
 leaflet(EisAlle) %>% addTiles() %>% setView(lat=48.2,16.4,zoom=12) %>% 
   addPolygons(
   fillColor= ~pal(EisAlle$min),weight = 2,
   opacity = 0.1,
   color = "white",
-  fillOpacity = 0.3)
+  fillOpacity = 0.3) %>% addMarkers(Eiscoor$long, Eiscoor$lat, icon = Stanizel)
