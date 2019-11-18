e1506b0deca2626959c1efb15b7c6ba41e902000
unknown <Clara.Schartner@gmail.com>
2019-11-18 15:24:04 +0100

    added isochrones

--- 01IceAdress.R
+++ 01IceAdress.R
@@ -1,4 +1,5 @@
 source("00libraries.R")
+# get adressees
 vegurl <- "https://www.veganista.at/shops"
 vegadr <- read_html(vegurl) %>%
   html_nodes('.txtNew') %>%
@@ -9,13 +10,47 @@ vegadr <- vegadr[vegadr %>% grepl("Wien", .)]
 vegadr <- substr(x = vegadr, 1, 
                  regexpr("Wien", vegadr) + 3)
 vegadr <- gsub("\n\n", " ", vegadr)
-Eis <- tibble(Adr= vegadr) %>%
-  geocode(Adr, "osm")
+# get coordinates for adresses (=geocode)
+Eiscoor <- tibble(Adr= vegadr) %>%
+  geocode(Adr, "osm") 
 
+Eis <- st_as_sf(Eiscoor, coords= c("long", "lat"), crs = 3857)
 
 # Visualize ---------------------------------------------------------------
 Stanizel <- makeIcon("StanizelIcon.png",
                           iconWidth = 20, iconHeight = 30)
-leaflet() %>% addTiles() %>% setView(lat=48.2,16.4,zoom=11) %>% 
-  addMarkers(Eis$long, Eis$lat, icon = Stanizel)
+leafIce <- leaflet() %>% addTiles() %>% setView(lat=48.2,16.4,zoom=12) %>% 
+  addMarkers(Eiscoor$long, Eiscoor$lat, icon = Stanizel)
+leafIce
 
+# Isochrone ---------------------------------------------------------------
+
+# Loop making isochrone for every ice cream
+# within the loop union
+bins <- seq(0,8,2)
+for(i in seq_along(Eiscoor$long)){
+EisIso <- osrmIsochrone(loc =c(Eiscoor$long[i], Eiscoor$lat[i]), 
+                        breaks = bins, 
+                        returnclass="sf")
+# rest, so that API does not block
+Sys.sleep(rnorm(1,150,10))
+ # combine the current polygon with the polygon containing the other ice creams
+if( i >1){
+  EisAlle <- EisIso %>% select(min)
+}
+EisAlle <- st_union(EisIso, EisAlle)
+# polygos from different ice creams overlap
+EisAlle$min <- apply(EisAlle,1, function(x)min(x$min, x$min.1))
+EisAlle<- select(EisAlle, min)
+}
+
+# visualise
+pal <- colorNumeric(
+  palette = "Reds",
+  domain = EisAlle$min)
+leaflet(EisAlle) %>% addTiles() %>% setView(lat=48.2,16.4,zoom=12) %>% 
+  addPolygons(
+  fillColor= ~pal(EisAlle$min),weight = 2,
+  opacity = 0.1,
+  color = "white",
+  fillOpacity = 0.3)
