03601df969151a06172c55af301d5bd0b22f5d50
unknown <Clara.Schartner@gmail.com>
2019-11-25 16:11:44 +0100

    filter out key words for locations

--- 02Wohnungsadressen.R
+++ 02Wohnungsadressen.R
@@ -1,8 +1,8 @@
-url <- "https://www.immobilienscout24.at/expose/5db8685a6bb5c22100be7fea"
+Anzei <- readLines("Anzeige.txt")
+Anzei <- strsplit(Anzei, ";") %>% unlist()
+antxt <- Anzei[5] %>% strsplit(" ") %>% unlist()
+# Keywords which could be followed by info about the location
+Offi <- grep("U1|U2|U3|U4|U6|U-Bahn|Bus|Haltestelle|Minuten|Fußweg|min|Meter|Straße|Weg|gasse", antxt)
+Loc <- lapply(Offi, function(x)antxt[(x-4):(x+6)]) %>% unlist() %>% unique() 
+Loc[!Loc %in% tm::stopwords("german")]
 
-
-woh <- read_html(vegurl) 
-plz <-woh%>%
-  html_nodes('.postalcode') %>%
-  html_text()
-anzText <- woh %>% html_text()
