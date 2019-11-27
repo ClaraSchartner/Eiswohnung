3a3f0e3a9f279754a8a6a006d3fa6355930fc947
unknown <Clara.Schartner@gmail.com>
2019-11-27 20:19:22 +0100

    made function, minor rewrite and fixing warning messages

--- 03Wohnungsadressen.R
+++ 03Wohnungsadressen.R
@@ -1,7 +1,13 @@
-
-url <- "https://www.immowelt.at/expose/2s8d74t"
-url <-"https://www.immowelt.at/expose/2qqw64v"
-url <- "https://www.immowelt.at/expose/2skw84t"
+source("00libraries.R")
+#url <- "https://www.immowelt.at/expose/2s8d74t"
+#url <-"https://www.immowelt.at/expose/2qqw64v"
+#url <- "https://www.immowelt.at/expose/2skw84t"
+Eisfunc <- function(url){
+  if(class(url)!="character"){
+    as.character(url)
+  }
+  Eis <- st_read("data/Eis.shp", quiet=TRUE)
+  bez <- st_read("data/BEZIRKSGRENZEOGDPolygon.shp", quiet=TRUE) %>% select(DISTRICT_C)
 text <- read_html(url) %>%
   html_nodes('.section_content.iw_right') %>%
   html_text()
@@ -22,7 +28,7 @@ strsplit(" ") %>%
   lapply(function(x)x[unlist(lapply(x, function(x)x != ""))])
 
 
-Anzei[[1]]<- Anzei[[1]][1:grep("Wien", Anzei[[1]])]
+Anzei[[1]]<- Anzei[[1]][1:grep("Wien", Anzei[[1]])[1]]
 
 if(length(Anzei[[1]])>2){
   Anzei[[1]] <- paste(Anzei[[1]], collapse = " ")
@@ -36,13 +42,12 @@ Offi <- grep(
   "U1|U2|U3|U4|U6|U-Bahn|Bus|Haltestelle|Minuten|Fußweg|min|Meter|Straße|strasse|platz|Weg|gasse|nahe|befindet|station", 
   Anzei[[2]], ignore.case = TRUE )
 Loc <- lapply(Offi, function(x)Anzei[[2]][(x-4):(x+6)]) %>% unlist() %>% unique() 
-# filter out numbers
-Loc <- Loc[Loc %>% as.numeric() %>% is.na()]
+
 # filter out stopwords
 Loc <- Loc[!Loc %in% tm::stopwords("german")]
 
 
-poi <- readRDS("poi.rds")
+poi <- readRDS("data/poi.rds")
 poi$name<-tolower(poi$name)
 Locations <- Loc[Loc %in% poi$name]
 Locations <- poi[which(poi$name==Locations),]
@@ -53,6 +58,7 @@ str <- Anzei[[2]][grep(
   Anzei[[2]], ignore.case = TRUE )]
 str <- str[!grepl( "tiefgarage|park|bahn|stell", str, ignore.case = TRUE)]
 str <- str[sapply(str, nchar)>5]
+# introduce more checks
 str <- paste(str, "Wien")
 
 Locations<- tibble(name= str) %>%
@@ -61,7 +67,11 @@ Locations<- tibble(name= str) %>%
   st_as_sf( coords= c("long", "lat"), crs = "+proj=longlat +datum=WGS84 +no_defs") %>% 
   rbind(Locations)
 
-Locations <- Locations[!Locations$name %>% duplicated(),]
+#Locations <- Locations[!Locations$name %>% duplicated(),]
+
+bez <- bez %>% filter(DISTRICT_C == as.numeric(Anzei[[1]][1])) 
+# maybe add small buffer later
+Locations <- Locations[bez,]
 
 
 }
@@ -69,8 +79,10 @@ if(nrow(Locations)==0){
   paste("No location information retrieved")
 } else{
 
-Eis <- st_read("data/Eis.shp")
+
 
 minD <- st_distance(Eis, Locations) %>% apply(2, min) %>% mean()
-minD}
-                                      
+
+minD
+}}
+#Eisfunc("https://www.immowelt.at/expose/2s8d74t")                                      
