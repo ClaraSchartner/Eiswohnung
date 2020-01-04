5e4bbbadcfcf272097a7b0b34bf63def7df9ccf1
unknown <Clara.Schartner@gmail.com>
2020-01-04 12:32:53 +0100

    function to get url to all listed links

--- 04getfurtherlinks.R
+++ 04getfurtherlinks.R
@@ -1,4 +1,4 @@
-urlov <- "https://www.immowelt.at/liste/wien/wohnungen/mieten?sort=createdate%2Bdesc"
+extracturls <- function(urlov = "https://www.immowelt.at/liste/wien/wohnungen/mieten?sort=createdate%2Bdesc"){
 
 html <- paste(readLines(urlov), collapse="\n")
 html <- str_match_all(html, "<a href=\"(.*?)\"")
@@ -8,17 +8,6 @@ posurl <- str_locate_all(html, "expose")
 
 posurl<- lapply(posurl, function(x)x[,2]) %>% unlist()
 urllinks <- substr(html, posurl+2, posurl+8)
-
 url <- paste0("https://www.immowelt.at/expose/", urllinks)
-text <- read_html(url[4]) %>%
-  html_nodes('.quickfacts.iw_left') %>%
-  html_text()
-text
-
-
-
-ub <- read_html(urlov) %>%
-  html_nodes('js-object   listitem_wrap ') %>%
-  html_text()
-ub
-<div class="js-object   listitem_wrap " data-gok="20DC3E3A-208A-4BF6-86F3-DB3ADDF96F6A" data-oid="2sa644z" data-estateid="59738249">
+url
+}
